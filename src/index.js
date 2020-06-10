15b32c835b239205cb97dcd5ff0303056afe1192
Cristian Bello Cervantes <cristiancervantes.cbc@gmail.com>
2020-06-10 00:33:53 -0500

    Implements solution for the challenge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const trialDivision = (number) => {
-    
+    let is_prime = true;
+    if(number > 1 &&  number % 1 === 0){
+        for(let i = number ;i > 1; i--){
+            if (number % i === 0 && number !== i){
+                is_prime = false
+            }
+        }
+    } else {
+       is_prime = false;
+    }
+
+    return is_prime;
   }
 
   module.exports = trialDivision;
